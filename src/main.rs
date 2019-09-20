df3c6444893e7045af6b379ea89f74aaf5ca67de
Romain Grossemy <romain.grossemy@sogeti.com>
2019-09-20 15:33:55 +0200

    Use rouille as webserver

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,8 @@ extern crate rand;
 
 use std::env;
 use rand::Rng;
-use std::net::TcpListener;
-use std::net::TcpStream;
-use std::io::Write;
-use std::io::Read;
+use rouille::Request;
+use rouille::Response;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -16,43 +14,15 @@ fn main() {
         "8080"
     }.to_string();
     let address = format!("127.0.0.1:{}",port);
-    let listener = match TcpListener::bind(address) {
-        Ok(u) => u,
-        _ => {
-            println!("Unable to open the connection on port {}, abort.", port);
-            return;
-        }
-    };
-
-    println!("Web server listening on :{}", port);
-
-    for stream in listener.incoming() {
-        let stream = stream.unwrap();
-
-        handle_connection(stream);
-    }
+    rouille::start_server(address, move |request| {
+            Response::text(joke_or_insult())
+    });
 }
 
-fn handle_connection(mut stream: TcpStream) {
-    let mut buffer = [0; 512];
-    stream.read(&mut buffer).unwrap();
-
-    let get = b"GET / HTTP/1.1\r\n";
-
-    if buffer.starts_with(get) {
-        let contents = joke_or_insult();
-
-        let response = format!("HTTP/1.1 200 OK\r\n\r\n{}\n", contents);
-
-        stream.write(response.as_bytes()).unwrap();
-        stream.flush().unwrap();
-    }
-}
 
 fn joke_or_insult() -> std::string::String {
     let mut rng = rand::thread_rng();
     let rdm = rng.gen_range(0, 2);
-    println!("Random number : {}", rdm);
     let res = if rdm == 0 {
         let client = reqwest::Client::new();
         let body = client.get("https://icanhazdadjoke.com/")
