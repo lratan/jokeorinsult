817bcb0bb345d4eac358587ba6ab656653cd93ba
Romain Grossemy <romain.grossemy@sogeti.com>
2019-09-20 16:16:04 +0200

    Refactor for better error management

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,8 @@ use rand::Rng;
 use rouille::Request;
 use rouille::Response;
 
+type Unwraper<T> = Result<T, Box<std::error::Error>>;
+
 fn main() {
     let args: Vec<String> = env::args().collect();
     let port = if (&args[1]).trim().parse::<f64>().is_ok() {
@@ -15,24 +17,24 @@ fn main() {
     }.to_string();
     let address = format!("127.0.0.1:{}",port);
     rouille::start_server(address, move |request| {
-            Response::text(joke_or_insult())
+            Response::text(joke_or_insult().unwrap_or("".to_string()))
     });
 }
 
 
-fn joke_or_insult() -> std::string::String {
+fn joke_or_insult() -> Unwraper<String> {
     let mut rng = rand::thread_rng();
     let rdm = rng.gen_range(0, 2);
     let res = if rdm == 0 {
         let client = reqwest::Client::new();
-        let body = client.get("https://icanhazdadjoke.com/")
-                .header("Accept", "text/plain")
-                    .send().unwrap().text().unwrap();
-        body
+        client.get("https://icanhazdadjoke.com/")
+              .header("Accept", "text/plain")
+              .send()?
+              .text()?
     } else {
-        let body = reqwest::get("https://insult.mattbas.org/api/insult").unwrap().text().unwrap();
-        body
-    }.to_string();
-    
-    return res;
+        reqwest::get("https://insult.mattbas.org/api/insult")?.text()?
+    }
+    .to_string();
+
+    Ok(res)
 }
